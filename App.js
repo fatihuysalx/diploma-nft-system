f9a83519fff0f1fb049b41948ec9f04a75c79c83
FatihUysal.x <117011627+fatihuysalx@users.noreply.github.com>
2025-01-06 01:30:44 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MintForm from "./MintForm";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>Diploma NFT Minting</h1>
+      <MintForm />
+    </div>
+  );
+}
+
+export default App;
